068a568ae277bcc4eef41946ee845d572f578083
Teo <teo@digitalcreative.asia>
2026-03-10 19:59:38 +0700

    Implement Astro Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://aatg.vn',
   output: 'static',
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   build: {
     inlineStylesheets: 'auto',
   },
